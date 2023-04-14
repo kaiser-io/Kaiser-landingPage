45bc0fa2acee368a699a0d06e4dbc9b05cffcf8b
Moeid Heidari <moeidheidari@comfortel.pro>
2023-04-14 14:00:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,9 +6,7 @@ const nextConfig = {
   output: 'export',
   outDir: 'out',
   
-  // If the repo name != // <organization>.github.io
-  // Unless images will not be loaded on gh-pages.
-  assetPrefix: isProd ? "/Kaiser-landingPage/" : "",
+  
 }
 
 module.exports = nextConfig
